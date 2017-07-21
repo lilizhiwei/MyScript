0dc0c6be12e48936f83fb7676474d3781e9e2501
lizhiwei <1548790965@qq.com>
2017-07-21 23:27:16 +0800

    上传代码

--- myfile.js
+++ myfile.js
@@ -1,6 +1,7 @@
 var login = {
 	userName: "13140023070",
-	passWord: "123456",
+	passWord: "li1548790965",
+	url: "https://yyddd.com/pc/login.html"
 };
 function lz(c){
   var a = c.indexOf("：")>-1?c.split("：")[1]:c;
