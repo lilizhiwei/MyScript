ec5d95e73b721636b9dbcf2690704af788ca98c2
lilizhiwei <1548790965@qq.com>
2017-07-22 10:50:23 +0800

    上传代码

--- myfile.js
+++ myfile.js
@@ -1,7 +1,8 @@
 var login = {
 	userName: "13140023070",
-	passWord: "li1548790965",
-	url: "https://yyddd.com/pc/login.html"
+	passWord: "123456",
+	url: "http://www.yyddd.com/pc/login.html",
+	url1: "http://www.yyddd.com/mobile"
 };
 function lz(c){
   var a = c.indexOf("：")>-1?c.split("：")[1]:c;
