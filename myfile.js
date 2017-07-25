09803b3e2ebe23557d7fdebf4e62837d2a7bcc84
lilizhiwei <1548790965@qq.com>
2017-07-25 18:28:32 +0800

    上传代码

--- myfile.js
+++ myfile.js
@@ -1,8 +1,8 @@
 var login = {
 	userName: "13140023070",
-	passWord: "li1548790965",
-	url: "https://www.yyddd.com/pc/login.html",
-	url1: "https://www.yyddd.com/mobile"
+	passWord: "123456",
+	url: "http://www.yyddd.com/pc/login.html",
+	url1: "http://www.yyddd.com/mobile"
 };
 function lz(c){
   var a = c.indexOf("：")>-1?c.split("：")[1]:c;
