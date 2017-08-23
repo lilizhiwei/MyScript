4d02ce6cbf6af1ceff91d9658804c937bc894d62
lilizhiwei <1548790965@qq.com>
2017-08-23 12:32:17 +0800

    上传代码

--- script/daoru/test_case/page/page_daoru.py
+++ script/daoru/test_case/page/page_daoru.py
@@ -1,5 +1,7 @@
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 from time import sleep
 import unittest
@@ -28,22 +30,26 @@ class login(object):
 		self.driver.find_element_by_id("loginAccount").send_keys(self.user)
 		self.driver.find_element_by_id("loginPassword").send_keys(self.pwd)
 		self.driver.find_element_by_xpath("//button[@type='submit']").click()
-		sleep(1)
 
 	#清空数据
 	def clickqingkong(self):
 		self.driver.find_element_by_xpath("//i[@class='site-menu-icon fa fa-cog']").click()
 		self.driver.find_element_by_link_text("通用设置").click()
-		sleep(1)
+		WebDriverWait(self.driver,30,0.5).until(EC.presence_of_element_located((By.CSS_SELECTOR,"span.padding-left-5")))
+		sleep(0.5)
 		self.driver.find_element_by_css_selector("span.padding-left-5").click()
-		sleep(2)
+		WebDriverWait(self.driver,30,0.5).until(EC.presence_of_element_located((By.XPATH,"//div/div/div[2]/div/button")))
+		sleep(0.5)
 		self.driver.find_element_by_xpath("//div/div/div[2]/div/button").click()
 		self.driver.find_element_by_xpath("//div[2]/div/button[2]").click()
 		self.driver.find_element_by_css_selector("input.layui-layer-input").send_keys(self.pwd)
 		self.driver.find_element_by_css_selector("a.layui-layer-btn0.pull-right").click()
-		sleep(3)
 
-	#进入客户页面
+	def bccg(self):
+		WebDriverWait(self.driver,30,0.5).until(EC.text_to_be_present_in_element((By.CSS_SELECTOR,".layui-layer-content.layui-layer-padding"),"保存成功！"))
+		sleep(0.5)
+
+	#进入页面
 	def clickkehu(self):
 		self.driver.find_element_by_xpath("//i[@class='site-menu-icon fa fa-group']").click()
 		self.driver.find_element_by_link_text("客户").click()
@@ -69,13 +75,20 @@ class login(object):
 		self.driver.find_element_by_xpath("//li[@data-power='147']//i").click()
 
 	def clickshang(self):
+		WebDriverWait(self.driver,30,0.5).until(EC.visibility_of_element_located((By.XPATH,"//i[@class='fa fa-cloud-upload fa-4x fa-fw']")))
+		sleep(0.5)
 		self.driver.find_element_by_xpath("//i[@class='fa fa-cloud-upload fa-4x fa-fw']").click()
 
 	def clickshang2(self):
 		self.driver.find_element_by_xpath("//i[@class='fa fa-refresh fa-4x fa-fw']").click()
 
 	def clicktijiao(self):
+		WebDriverWait(self.driver,30,0.5).until(EC.text_to_be_present_in_element((By.CSS_SELECTOR,".btn.btn-primary.margin-top-15.sender"),"确认提交"))
+		sleep(0.5)
 		self.driver.find_element_by_css_selector("i.fa.fa-check.fa-fw").click()
+
+	def yanzheng(self):
+		WebDriverWait(self.driver,30,0.5).until(EC.text_to_be_present_in_element((By.CSS_SELECTOR,"div.layui-layer-content"),"导入完毕"))
 
 	def clickwanbi(self):
 		return self.driver.find_element_by_css_selector("div.layui-layer-content").text
