405b2e6dd6ba258e98af9398f1011c2624dfdbbc
lilizhiwei <1548790965@qq.com>
2017-08-28 18:19:04 +0800

    上传代码

--- script/qx/gjqx/test_kh.py
+++ script/qx/gjqx/test_kh.py
@@ -14,8 +14,30 @@ class loginTest(unittest.TestCase):
 		self.driver = webdriver.Chrome()
 
 	def test_1kehu(self):
-		#导入正确
+		#打开第一页面
 		gjqx(self.driver).user_login()
+		
+		#获取第一页面handle信息
+		sreach_windows = self.driver.current_window_handle
+
+		#打开第二页面
+		self.driver.execute_script('window.open("https://www.baidu.com")')
+		sleep(2)
+
+		#获取所有页面信息
+		all_handles = self.driver.window_handles
+
+		#切回第一页面
+		for handle in all_handles:
+			if handle == sreach_windows:
+				self.driver.switch_to.window(handle)
+				sleep(5)
+
+		#切回第二页面
+		for handle in all_handles:
+			if handle != sreach_windows:
+				self.driver.switch_to.window(handle)
+				sleep(5)
 
 	def test_2kehu(self):
 		#导入正确
