9632248b337e162d8888ddfc89baec74a630dc4b
lilizhiwei <1548790965@qq.com>
2017-10-13 18:03:58 +0800

    上传代码

--- myfile.js
+++ myfile.js
@@ -1,9 +1,9 @@
 var login = {
 	userName: "13140023070",
 	passWord: "123456",
-	url: "https://www.yyddd.com/pc/login.html",
-	url1: "https://www.yyddd.com/mobile",
-	url2: "https://www.yyddd.com/cash/#page/login"
+	url: "http://www.yyddd.com/pc/login.html",
+	url1: "http://www.yyddd.com/mobile",
+	url2: "http://www.yyddd.com/cash/#page/login"
 };
 function lz(c){
   var a = c.indexOf("：")>-1?c.split("：")[1]:c;
